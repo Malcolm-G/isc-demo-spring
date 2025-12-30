d157cfc47609b4cf77f900ba4df347515c684c02
Malcolm-G <malcolm.githuka@gmail.com>
2025-12-30 16:44:20 +0300

    Create project
    - Add a consumer and a provider to test comunication between the 2

---
+++ consumer/src/test/java/com/malcolm/demo/isc/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malcolm.demo.isc.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
