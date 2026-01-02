4be823705e19a7d7eff87b6776f6f501456d456b
Malcolm-G <malcolm.githuka@gmail.com>
2026-01-02 18:46:09 +0300

    Update HttpInterface to use Eureka server

--- consumer/src/main/java/com/malcolm/demo/isc/consumer/httpinterface/HttpInterfaceConfig.java
+++ consumer/src/main/java/com/malcolm/demo/isc/consumer/httpinterface/HttpInterfaceConfig.java
@@ -11,18 +11,40 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 @Configuration
 public class HttpInterfaceConfig {
 
+	/**
+	 * We are using the load-balanced bean defined at
+	 * {@link com.malcolm.demo.isc.consumer.webclient.WebClientConfig#loadBalancedWebClientBuilder()}
+	 * <br>
+	 * <br>
+	 * You can define another loadBalanced WebClient Builder bean but use @Qualifier
+	 * or @Primary
+	 * 
+	 * @param loadBalancedRestClientBuilder
+	 * @return
+	 */
 	@Bean
-	ProviderHttpInterface webClientHttpInterface() {
-		WebClient webClient = WebClient.create("http://localhost:8081/");
+	ProviderHttpInterface webClientHttpInterface(WebClient.Builder builder) {
+		WebClient webClient = builder.baseUrl("http://provider").build();
 		WebClientAdapter adapter = WebClientAdapter.create(webClient);
 		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
 		ProviderHttpInterface service = factory.createClient(ProviderHttpInterface.class);
 		return service;
 	}
-	
+
+	/**
+	 * We are using the load-balanced bean defined at
+	 * {@link com.malcolm.demo.isc.consumer.restclient.RestClientConfig#loadBalancedRestClientBuilder()}
+	 * <br>
+	 * <br>
+	 * You can define another loadBalanced RestClient Builder bean but
+	 * use @Qualifier or @Primary
+	 * 
+	 * @param loadBalancedRestClientBuilder
+	 * @return
+	 */
 	@Bean
-	ProviderHttpInterface restClientHttpInterface() {
-		RestClient  restClient = RestClient.create("http://localhost:8081/");
+	ProviderHttpInterface restClientHttpInterface(RestClient.Builder loadBalancedRestClientBuilder) {
+		RestClient restClient = loadBalancedRestClientBuilder.baseUrl("http://provider").build();
 		RestClientAdapter adapter = RestClientAdapter.create(restClient);
 		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
 		ProviderHttpInterface service = factory.createClient(ProviderHttpInterface.class);
