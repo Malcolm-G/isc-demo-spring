92497d3f202b87e2f970d85ad3e114b09676919e
Malcolm-G <malcolm.githuka@gmail.com>
2026-01-02 17:56:22 +0300

    Use Eureka for WebClient http calls

--- consumer/src/main/java/com/malcolm/demo/isc/consumer/webclient/WebClientConfig.java
+++ consumer/src/main/java/com/malcolm/demo/isc/consumer/webclient/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.malcolm.demo.isc.consumer.webclient;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,9 +8,43 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+	// Unlike RestTemplate we need a LoadBalanced Builder
 	@Bean
-	WebClient webClient(WebClient.Builder builder) {
-		return builder.baseUrl("http://localhost:8081").build();
+	@LoadBalanced
+	WebClient.Builder loadBalancedWebClientBuilder() {
+		return WebClient.builder();
 	}
 
+	@Bean
+	WebClient webClient(WebClient.Builder builder) {
+		/*
+		 * Use the code commented to change the resolver. WebClient uses Netty's
+		 * resolver which has issues with OS hostnames like .mshome.net This will use
+		 * JVM's resolver which isn't aync like Netty's but it works. And although it
+		 * may block the eventloop slightly it still is unblocking for the main request
+		 * 
+		 * HttpClient httpClient =
+		 * HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE); return
+		 * builder.baseUrl("http://provider").clientConnector(new
+		 * ReactorClientHttpConnector(httpClient)).build();
+		 */
+		return builder.baseUrl("http://provider").build();
+	}
+
+	/*
+	 * @Bean WebClient webClient2(WebClient.Builder builder) {
+	 * 
+	 * // Use the code commented to change the resolver. WebClient uses Netty's //
+	 * resolver which has issues with OS hostnames (like .mshome.net) This will use
+	 * // JVM's resolver which isn't aync like Netty's but it works. And although it
+	 * // may block the eventloop slightly it still is unblocking for the main
+	 * request
+	 * 
+	 * HttpClient httpClient =
+	 * HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE); return
+	 * builder.baseUrl("http://provider").clientConnector(new
+	 * ReactorClientHttpConnector(httpClient)).build();
+	 * 
+	 * }
+	 */
 }
